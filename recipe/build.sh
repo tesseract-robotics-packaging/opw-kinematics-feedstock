6a0b2dbc1c0e05778bf791c140d7cc2970716571
John Wason <wason@wasontech.com>
2026-05-29 22:24:30 -0400

    Remove cmake_target_args

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,7 @@ set -e
 mkdir -p src
 tar xf source.tar.gz --strip-components=1 -C src
 
-if [[ "$target_platform" == "linux-aarch64" ]]; then
-  CMAKE_TARGET_ARGS=-DCMAKE_SYSTEM_NAME2=aarch64
-fi
-
-cmake ${CMAKE_ARGS} ${CMAKE_TARGET_ARGS} \
+cmake ${CMAKE_ARGS} \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_VERBOSE_MAKEFILE=ON \
   -S src \
